59ab63c7ffb3e03eff5190fc6fa834b5cd7e39de
maryam ahmadi raqeb <93914766+Mryrghb@users.noreply.github.com>
2024-02-07 14:46:12 +0330

    todo.js

--- Routes/CRUD/todo.js
+++ Routes/CRUD/todo.js
@@ -6,17 +6,19 @@ const fs = require('fs');
 
 const dataPath = './Details/todo.json' 
 
-
+// write in json
 const savetodoUsers = (data) => {
     const stringifyData = JSON.stringify(data)
     fs.writeFileSync(dataPath, stringifyData)
 }
 
+//read of json 
 const gettodoList = () => {
     const jsonData = fs.readFileSync(dataPath)
     return JSON.parse(jsonData)    
 }
 
+// find one todo by taskName - for Read
 const getOneOfTodo = ({
     userId,
     taskId,
@@ -36,7 +38,7 @@ const getOneOfTodo = ({
 
 
 
-// read json data
+// create strat url 
 todoListRoutes.get('/todo', (req, res) => {
     fs.readFile(dataPath, 'utf8', (err, data) => {
       if (err) {
@@ -48,6 +50,7 @@ todoListRoutes.get('/todo', (req, res) => {
   });
 
 
+//create new Todo ------------------------------------------------------------------------ C
   todoListRoutes.post('/todo/addTask', (req, res) => {
    
     const existTask = gettodoList()
@@ -56,16 +59,17 @@ todoListRoutes.get('/todo', (req, res) => {
     console.log({existTask, body: req.body});
 
     saveAccountData(existTask);
-    return res.send({success: true, msg: 'account data added successfully'})
+    return res.send({success: true, msg: 'todo data added successfully'})
 })
 
 
-// Read - get all accounts from the json file
+// Read all todo -------------------------------------------------------------------------- R
 todoListRoutes.get('/todo/list', (req, res) => {
   const tasks = gettodoList()
   res.send(tasks)
 })
 
+//read One todo 
 todoListRoutes.get('/todo/taskName/:taskName', (req, res) => {
   console.log({params: req.params})
   const foundedTask = getOneOfTodo({taskName: req.params["taskName"]})
@@ -73,27 +77,26 @@ todoListRoutes.get('/todo/taskName/:taskName', (req, res) => {
 })
 
 
-//do not update!!!!
-// Update - using Put method
-todoListRoutes.put('/account/:name', (req, res) => {
+// Update todo -------------------------------------------------------------------------------- U
+todoListRoutes.put('/todo/:taskName', (req, res) => {
 
-  fs.readFile(dataPath, "utf8" , (err,data)=>{
-    if (err){
-      console.log("error in reading accounts information!")
-    } else{
-      const existAcc = getAccounts()
-      const foundAcc = req.params['id'];
-      existAcc[foundAcc] = req.body;
-      saveAccountData(existAcc);
-      res.send("update details")
+  const existTodoList = gettodoList();
+  const TodoData = req.body;
+  if(TodoData.taskName === null || TodoData.taskDetail === null || TodoData.done === null ) {
+      return res.status(401).send({error: true, msg: 'todoList Data missing'});
+  }
 
-      // const existUserss = getAccounts()
-      // getAccounts.put({...req.body})
-      // saveAccountData(getAccounts)
-      // res.send("succes!")
-   
-    }
+  existTodoList.find(todo => {
+      if(todo.taskName === TodoData.taskName) {
+        todo.done = TodoData.done;
+      }
   })
+
+  console.log(JSON.stringify(existTodoList));
+
+
+  savetodoUsers(existTodoList);
+  res.send({success: true, msg: 'todoList data updated successfully'});
 });
 
 
@@ -101,52 +104,16 @@ todoListRoutes.put('/account/:name', (req, res) => {
 
 
 
-//do not delete!!!
-//delete - using delete method
-todoListRoutes.delete('/account/delete/:id', bodyParser.json(),(req, res) => {
-  fs.readFile(dataPath, "utf8", (err, data)=>{
-    var exsitAccount = getAccounts()
-    const user = req.params['id'];
-    delete exsitAccount[user];
-    saveAccountData(exsitAccount);
-    res.send(`ok`)}, true);
+
+//delete todo ---------------------------------------------------------------------------------- D
+todoListRoutes.delete('/todo/:taskName', bodyParser.json(),(req, res) => {
+
+    const existTodoList = gettodoList();
+    const todoListData = req.params;
+    const newListOfTodo = existTodoList.filter(todo => todo.taskName !== todoListData.taskName)
+    savetodoUsers(newListOfTodo);
+    res.send({success: true, msg: 'this todo data deleted successfully'});
   })
 
-
-  //S1
-  // fs.readFile(dataPath, "utf8", (err, data)=>{
-  // let exsit = getAccounts();
-  // let doneDel = getOneAcountById({id: req.params['id']})
-  //  remo(exsit[doneDel])
-  // saveAccountData(exsit)
-  // res.send({exsit})
-  // })
-  // delete foundedAcount[{foundedById}]
-  // saveAccountData(existAccounts)
-  // console.log({foundedById})
-
-  //S2
-  // const exitAcc = getAccounts()
-  // fs.readFile(dataPath, "utf8", (err, data)=>{
-  //   let exitAcc = getAccounts()
-  //   const found = req.params['id'];
-  //   delete exitAcc[found]
-  //   saveAccountData(getAccounts);
-  //   res.send("succes")
-  // })
-
-  //S3
-  // const foundedAcount = getOneAcount()
-  // const acc = getAccounts.find(ac => ac.id === req.params.id);
-  // acc = ({...req.body});
-  //  fs.readFile(dataPath, 'utf8', (err, data) => {
-  //   var existAccounts = getAccounts()
-
-  //   const userId = req.params['id'];
-
-  //   delete existAccounts[userId];  
-  //   saveAccountData(existAccounts);
-  //   res.send(`accounts with id ${userId} has been deleted`)
-  // }, true);
 
 module.exports = todoListRoutes
